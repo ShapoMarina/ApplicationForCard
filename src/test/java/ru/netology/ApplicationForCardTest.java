4260b16d2e32a55341c9c488f6a4dea3074cac4e
Marina Shapovalova <snyzinka@gmail.com>
2023-05-28 19:33:49 +0300

    Second commit

--- src/test/java/ru/netology/ApplicationForCardTest.java
+++ src/test/java/ru/netology/ApplicationForCardTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static com.codeborne.selenide.files.DownloadActions.click;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -22,6 +23,11 @@ public class ApplicationForCardTest {
     @BeforeEach
     void setUp() {
         driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);;
 
     }
 
